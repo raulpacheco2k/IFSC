5cf124827a8425c430181631c8ad7b287cdd6ae1
Raul Pacheco <eu@raulpacheco.com.br>
2021-06-03 14:57:44 -0300

    Add methods setter and getters

--- Object-Oriented Programming/Practical Activities/28-05-2021/Activity 2/src/Person.java
+++ Object-Oriented Programming/Practical Activities/28-05-2021/Activity 2/src/Person.java
@@ -1,19 +1,64 @@
 public class Person {
-    protected String name;
-    protected String gender;
-    protected String eyeColor;
-    protected float height;
-    protected float weight;
+    private String name;
+    private String gender;
+    private String eyeColor;
+    private float height;
+    private float weight;
 
     public Person(String name, String gender, String eyeColor, float height, float weight){
-        this.name = name;
-        this.gender = gender;
-        this.eyeColor = eyeColor;
-        this.height = height;
-        this.weight = weight;
+        this.setName(name);
+        this.setGender(gender);
+        this.setEyeColor(eyeColor);
+        this.setHeight(height);
+        this.setWeight(weight);
     }
 
     public float imc(){
-        return weight / (height * height);
+        return getWeight() / (getHeight() * getHeight());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Person setGender(String gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public Person setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+        return this;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public Person setHeight(float height) {
+        this.height = height;
+        return this;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public Person setWeight(float weight) {
+        this.weight = weight;
+        return this;
     }
 }
